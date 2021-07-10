103f20ff6f306ef6c33e96613742045cc83e5e72
JohnserfSeed <40727745+Johnserf-Seed@users.noreply.github.com>
2021-07-11 00:24:24 +0800

    修改搜索框样式

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -5,15 +5,15 @@ Component({
     selectedColor: "#3cc51f",
     //导航栏
     list: [{
-      "text": "越剧",
+      "text": "越唱吧",
       "pagePath": "/pages/index/index",
       "iconPath": "/images/toolbar/tabbar_icon_theatre_default.png",
       "selectedIconPath": "/images/toolbar/tabbar_icon_theatre_active.png",
         dot: true
       },
       {
-        "text": "越地小吃",
-        "pagePath": "/pages/im/im",
+        "text": "越有味",
+        "pagePath": "/pages/yyw/yyw",
         "iconPath": "/images/toolbar/tabbar_icon_food_default.png",
         "selectedIconPath": "/images/toolbar/tabbar_icon_food_active.png",
           dot: true
