83d11138e8da7a311b7e925c5a6617fc29061164
johnserf-seed <2080979987@qq.com>
2021-08-01 16:32:33 +0800

    启动时下载字体

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,11 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.loadFontFace({
+        family: '方正字迹-海体正楷 简',
+        global: true,
+        source: 'url("https://music-0gtd2khj7f8707a2-1257133085.tcloudbaseapp.com/font/%E6%96%B9%E6%AD%A3%E5%AD%97%E8%BF%B9-%E6%B5%B7%E4%BD%93%E6%AD%A3%E6%A5%B7%20%E7%AE%80%20%E5%B8%B8%E8%A7%84.TTF")'
+      })
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
